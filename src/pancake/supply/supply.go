5ad3a8bef5db14c28c5ba5c5dcc8bd6d7c769ac0
Dr Nic Williams <drnicwilliams@gmail.com>
2019-05-25 19:09:28 +1000

    update to cf-pancake 0.2.0 with .tar.xz asset

--- src/pancake/supply/supply.go
+++ src/pancake/supply/supply.go
@@ -2,6 +2,8 @@ package supply
 
 import (
 	"io"
+	"path/filepath"
+	"os"
 
 	"github.com/cloudfoundry/libbuildpack"
 )
@@ -50,6 +52,18 @@ func (s *Supplier) Run() error {
 	s.Log.Info("Using cf-pancake version %s", pancake.Version)
 
 	if err := s.Installer.InstallDependency(pancake, s.Stager.DepDir()); err != nil {
+		return err
+	}
+
+	// Find unpacked "etcd-v3.3.13-linux-amd64/etcd" file
+	pancakeBin, err := filepath.Glob(filepath.Join(s.Stager.DepDir(), "cf-pancake*"))
+	if err != nil {
+		return err
+	}
+
+	// Rename and move into bin/etcd, which will be in $PATH
+	err = os.Rename(pancakeBin[0], filepath.Join(s.Stager.DepDir(), "bin", "cf-pancake"))
+	if err != nil {
 		return err
 	}
 	return nil
